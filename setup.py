c322adf9bae528b9d21ea6d9423aa86642ebdf6c
ChunelFeng <chunel@foxmail.com>
2025-05-03 00:13:41 +0800

    cicd: v0.0.17

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@
 import glob
 from setuptools import setup, Extension
 import pybind11
+import os
 
 __CUPLOADER_NAME__ = "PyCUploader"
-__CUPLOADER_VERSION__ = "0.0.16"
+__CUPLOADER_VERSION__ = "0.0.17"
 __CUPLOADER_AUTHOR__ = "Chunel"
 __CUPLOADER_AUTHOR_EMAIL__ = "chunel@foxmail.com"
 __CUPLOADER_DESCRIPTION__ = "Chunel test pypi uploader"
@@ -13,7 +14,7 @@ __CUPLOADER_LICENSE__ = "Apache 2.0"
 
 _sources = ['PyUploadDemo.cpp'] + glob.glob("src/**/*.cpp", recursive=True)
 _extra_compile_args = ["-pthread", "-std=c++11", '-fvisibility=hidden']
-_include_dirs = [pybind11.get_include(), "./", "./src/"]
+_include_dirs = [pybind11.get_include(), os.getcwd(), os.path.join(os.getcwd(), 'src')]
 _ext_modules = [
     Extension(
         name=__CUPLOADER_NAME__,
