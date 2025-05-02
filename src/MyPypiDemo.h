cdd1e8d7a04f5ef34465e8e4a57f19320082a956
ChunelFeng <chunel@foxmail.com>
2025-05-02 16:46:14 +0800

    cicd: demo test

---
+++ src/MyPypiDemo.h
@@ -0,0 +1,22 @@
+/***************************
+@Author: Chunel
+@Contact: chunel@foxmail.com
+@File: MyPypiDemo.h
+@Time: 2025/5/2 14:50
+@Desc: 
+***************************/
+
+#ifndef CPYPIUPLOADDEMO_MYPYPIDEMO_H
+#define CPYPIUPLOADDEMO_MYPYPIDEMO_H
+
+
+class MyPypiDemo {
+public:
+    explicit MyPypiDemo() = default;
+    virtual ~MyPypiDemo() = default;
+
+    int add(int a, int b);
+};
+
+
+#endif //CPYPIUPLOADDEMO_MYPYPIDEMO_H
